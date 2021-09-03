57bd2d7f11837261c7e6109d211bd2894d4ec4ed
nickschmitt <nickschmitt@gmail.com>
2021-09-03 16:11:46 -0700

    CORS bugs

--- index.js
+++ index.js
@@ -1,277 +1,64 @@
 require('dotenv').config()
-const express = require('express')
-const layouts = require('express-ejs-layouts')
-const session = require('express-session')
-const passport = require('./config/ppConfig')
-const isLoggedIn = require('./middleware/isLoggedIn')
-const app = express()
-const flash = require('connect-flash')
-const axios = require('axios')
-const db = require('./models')
-const chalk = require('chalk')
-const methodOverride = require('method-override')
-
-app.set('view engine', 'ejs')
-
-// app.use(require('morgan')('dev'))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(__dirname + '/public'))
-app.use(layouts)
-app.use(methodOverride('_method'))
+const cookieSession = require("cookie-session");
+const express = require("express");
+const app = express();
+const port = 4000;
+const passport = require("passport");
+const passportSetup = require("./config/ppconfig");
+const session = require("express-session");
+const authRoutes = require("./routes/auth");
+// const keys = require("./config/keys");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-    })
+	cookieSession({
+		name: "session",
+		keys: "examplecookie",
+		maxAge: 60 * 60 * 1000 // 1 hour
+	})
 )
-app.use(flash())
+
+app.use(cookieParser())
 
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use((req, res, next) => {
-    // before every route, attach the flash messages and current user to res.locals
-    // res.locals.alerts = req.flash()
-    res.locals.currentUser = req.user
-    next()
-})
-
-// app.get('/', (req, res) => {
-//   {
-//     res.render('./auth/login')
-//   }
-// })
-
-app.get('/', isLoggedIn, (req, res) => {
-    {
-        res.render('index')
-    }
-})
-
-
-
-//--------checks if user is signed in and grabs all comments from comment database 
-
-
-app.get('/profile', isLoggedIn, async (req, res) => {
-	
-    const comments = await db.comment.findAll({
-		where: {
-			userId: req.user.id
-		}
+app.use(
+	cors({
+		origin: "http://localhost:3000", // allow to server to accept request from different origin
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		credentials: true // allow session cookie from browser to pass through
 	})
-	console.log(comments)
-	res.render('profile', {
-		comments,
-		user: req.user
+)
+
+app.use('/auth', authRoutes)
+
+const authCheck = (req, res, next) => {
+	if (!req.user) {
+		res.status(401).json({
+			authenticated: false,
+			message: "user has not been authenticated"
+		})
+	} else {
+		next()
+	}
+}
+
+app.get('/', authCheck, (req, res) => {
+	res.status(200).json({
+		authenticated: true,
+		message: "Authentication successful",
+		user: req.user,
+		cookies: req.cookies
 	})
 })
 
-
-
-//--------
-
-app.get('/results', (req, res) => {
-    const queryString = {
-        params: {
-            q: req.query.track,
-            type: req.query.type,
-            limit: req.query.limit,
-        },
-    }
-    axios
-        .get(
-            `https://api.spotify.com/v1/search?q=${queryString.params.q}&type=${queryString.params.type}&limit=${queryString.params.limit}`, {
-                headers: {
-                    Authorization: `Bearer ${req.user.access}`,
-                },
-            }
-        )
-        .then(function(response) {
-            res.render('results', {
-                data: response.data,
-                params: queryString.params,
-            })
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-})
-
-
-
-//--------
-
-
-app.get('/tracks/:id', function(req, res) {
-    const queryString = {
-        params: {
-            id: req.params.id,
-        },
-    }
-
-    axios
-        .get(`https://api.spotify.com/v1/tracks/${queryString.params.id}`, {
-            headers: {
-                Authorization: `Bearer ${req.user.access}`,
-            },
-        })
-        .then(function(spotifyResponse) {
-            db.comment
-                .findAll({
-                    where: { trackId: spotifyResponse.data.id },
-                    include: [db.user],
-                })
-                .then((allComments) => {
-                    // res.send(allComments)
-                    res.render('track', {
-                        comments: allComments,
-                        track: spotifyResponse.data,
-                    })
-                })
-                .catch((error) => {
-                    console.log(error)
-                })
-        })
-})
-
-
-
-//--------
-
-
-
-app.get('/artists/:id', function(req, res) {
-    const queryString = {
-        params: {
-            id: req.params.id,
-        },
-    }
-
-    axios
-        .get(`https://api.spotify.com/v1/artists/${queryString.params.id}`, {
-            headers: {
-                Authorization: `Bearer ${req.user.access}`,
-            },
-        })
-        .then(function(artistResponse) {
-            axios
-                .get(
-                    `https://api.spotify.com/v1/artists/${queryString.params.id}/top-tracks?country=US`, {
-                        headers: {
-                            Authorization: `Bearer ${req.user.access}`,
-                        },
-                    }
-                )
-                .then(function(topTracksResponse) {
-                    res.render('artist', {
-                        artist: artistResponse.data,
-                        tracks: topTracksResponse.data.tracks,
-                    })
-                })
-                // db.comment
-                //   .findAll({ where: { trackId: spotifyResponse.data.id } })
-                //   .then((allComments) => {
-                //     res.render('track', {
-                //       comments: allComments,
-                //       track: spotifyResponse.data,
-                //     })
-                //   })
-        })
-})
-
-
-//-------- add usnique user to comment
-
-app.post('/track', (req, res) => {
-    db.user
-        .findOne({ where: { spotifyId: req.user.spotifyId } })
-        .then(function(user) {
-            user
-                .createComment({
-                    text: req.body.text,
-                    userId: req.user.id,
-                    trackId: req.body.spotifyId,
-                })
-                .then(function(comment) {
-                    user.addComment(comment)
-                    res.redirect('back')
-                })
-        })
-})
-
-
-
-// *** EDIT COMMENT
-app.get('/edit/:id', (req, res) => {
-    const id = req.params.id
-    console.log(chalk.red(id))
-    db.comment
-        .findOne({
-            where: { id: id },
-        })
-        .then((comment) => {
-            res.render('editcomment', {
-                comment: comment,
-            })
-        })
-})
-
-
-
-app.put('/comments/:id', (req, res) => {
-    const id = req.body.commentId
-
-    db.comment
-        .update({ text: req.body.text }, {
-            where: { id: id },
-        })
-        .then((comment) => {
-            res.redirect('/')
-        })
-})
-
-
-
-
-// *** DELETE COMMENT
-app.delete('/comment/:id', (req, res) => {
-    const id = req.params.id
-    db.comment
-        .destroy({
-            where: { id: id },
-        })
-        .then((deletedComment) => {
-            res.redirect('back')
-        })
-})
-
-//-------- renders about page 
-
-app.get('/about', isLoggedIn, function(req, res) {
-    res.render('about')
-})
-
-
-//-------- logs  user out 
-
-app.get('/logout', function(req, res) {
-    req.logout()
-    res.redirect('/')
-})
-
-
-
-app.use('/auth', require('./routes/auth'))
-
-var server = app.listen(process.env.PORT || 3000, () =>
-    console.log(
-        `🎧You're listening to the smooth sounds of port ${
-      process.env.PORT || 3000
-    }🎧`
-    )
+const server = app.listen(process.env.PORT || 4000, () =>
+	console.log(
+		`🎧You're listening to the smooth sounds of port ${process.env.PORT || 4000
+		}🎧`
+	)
 )
 
 module.exports = server
--- routes/auth.js
+++ routes/auth.js
@@ -2,77 +2,44 @@ const express = require('express')
 const passport = require('../config/ppConfig')
 const db = require('../models')
 const router = express.Router()
+const CLIENT_URL = "http://localhost:3000"
 
-router.get('/spotify', passport.authenticate('spotify', {
-	scope: ['streaming', 'user-read-private', 'user-top-read', 'user-read-recently-played', 'user-follow-read']
-}))
-
-router.get(
-  '/spotify/callback',
-  passport.authenticate('spotify', { 
-	  failureRedirect: '/login',
-	  scope: ['streaming', 'user-read-private', 'user-top-read', 'user-read-recently-played', 'user-follow-read']
-	}),
-  function (req, res) {
-    // console.log('test test test')
-    // Successful authentication, redirect home.
-    res.redirect('/')
-  }
-)
-
-// router.get('/signup', (req, res) => {
-//   res.render('auth/signup');
-// });
-
-// router.post('/signup', (req, res) => {
-//   // find or create a user, providing the name and password as default values
-//   db.user.findOrCreate({
-//     where: {
-//       email: req.body.email
-//     }, defaults: {
-//       name: req.body.name,
-//       password: req.body.password
-//     }
-//   }).then(([user, created]) => {
-//     if (created) {
-//       // if created, success and login
-//       console.log('user created');
-//       passport.authenticate('local', {
-//         successRedirect: '/',
-//         successFlash: 'Account created and logged in'
-//       })(req, res);
-//     } else {
-//       // if not created, the email already exists
-//       req.flash('error', 'Email already exists');
-//       res.redirect('/auth/signup');
-//     }
-//   }).catch(error => {
-//     // if an error occurs, let's see what the error is
-//     req.flash('error', error.message);
-//     res.redirect('/auth/signup');
-//   });
-// })
-
-router.get('/login', (req, res) => {
-  db.comment.findAll().then((allComments) => {
-    //   console.log(req.user)
-    res.render('auth/login', {
-      comments: allComments,
-    })
-  })
+router.get('/login/success', (req, res) => {
+	console.log(req.user)
+	if (req.user) {
+		res.json({
+			success: true,
+			message: "User Authenticated",
+			user: req.user,
+			cookies: req.cookies
+		})
+	} else {
+		res.status(400).json({
+			message: "Authentication failed",
+			user: null
+		})
+	}
 })
 
-// router.post('/login', passport.authenticate('local', {
-//   successRedirect: '/',
-//   failureRedirect: '/auth/login',
-//   failureFlash: 'Invalid username and/or password',
-//   successFlash: 'You have logged in'
-// }));
+router.get('/login/failed', (req, res) => {
+	res.status(401).json({
+		success: false,
+		message: "Authentication failed"
+	})
+})
 
-// router.get('/logout', (req, res) => {
-//   req.logout()
-//   req.flash('success', 'You have logged out')
-//   res.redirect('/')
-// })
+router.get('/spotify', passport.authenticate('spotify'))
+
+// router.get('/spotify', (passport.authenticate('spotify', {
+// 	scope: ['streaming', 'user-read-private', 'user-top-read', 'user-read-recently-played', 'user-follow-read']
+// })))
+
+router.get(
+	'/spotify/callback',
+	passport.authenticate('spotify', {
+		successRedirect: CLIENT_URL,
+		failureRedirect: "auth/login/failed",
+	})
+)
 
 module.exports = router
