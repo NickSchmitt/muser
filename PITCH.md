4c26ec04800ce216700a88720dd29a87473e6b62
nickschmitt <nickschmitt@gmail.com>
2021-09-07 08:57:16 -0700

    Rearranging SSR and React artefacts

---
+++ PITCH.md
@@ -0,0 +1,46 @@
+# Muser
+Reddit threads and Youtube comment sections can be great places to discuss your favorite songs with others, but sometimes it can be difficult to find the songs you want to discuss; the scope of these platforms is too large to serve such a specific purpose well. This inconvenience inspired the creation of Muser, a discussion platform for a global community of music enthusiasts. Using your Spotify credentials, you can preview every song from the Spotify library, and comment on those songs.
+
+- As a user, I want to be able to sign up
+- As a user, I want to be able to search for a song
+- As a user, I want to be able to play the song
+- I want to be able to comment on a song
+- I want to be able to view my comments (profile) and other users comments
+- user next to the comment
+- Search by artist
+- add name to the user model
+
+
+
+# User Flow
+
+<img src="public/img/WIREFRAME-ERD-USERFLOW/Screen Shot 2020-10-30 at 7.47.34 AM.png" alt="home"/>
+
+
+# The homepage
+
+<img src="https://imgur.com/VEWQEtG.png" alt="home"/>
+
+- Once you sign in with your Spotify account you will land on a page where you can search by artist, song, or album.
+
+<img src="https://imgur.com/iYhwy1E.png" alt="home page"/>
+
+- After the page with all the relevant results loads, you can choose what search to get more information on.
+<img src="https://imgur.com/dWfZ3VK.png" alt="search results"/>
+
+- This brings you to a comment page where you can see what other people have written, add your own comments, and update your comments.
+<img src="https://imgur.com/apGVHRe.png" alt="song details"/>
+
+
+
+TBA functionality:
+- The ability to like a comment.
+- The ability to favorite songs.
+- The ability to sync your Spotify playlists within Muser.
+- Timestamped comments.
+- An about section for each artist and album.
+- A chat/ livestream feature.
+- A social credit system to encourage positivity.
+
+- Credit:
+JMprez, Spotify. 
--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 require('dotenv').config()
-const express = require("express");
-const app = express();
-const port = 4000;
-const passport = require("passport");
-const passportSetup = require("./config/ppconfig");
+const express = require('express')
+const layouts = require('express-ejs-layouts')
+const session = require('express-session')
+const passport = require('./config/ppConfig')
 const isLoggedIn = require('./middleware/isLoggedIn')
-const session = require("express-session");
-const authRoutes = require("./routes/auth");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const flash = require('connect-flash');
-const { default: axios } = require('axios');
+const app = express()
+const flash = require('connect-flash')
+const axios = require('axios')
+const db = require('./models')
+const chalk = require('chalk')
+const methodOverride = require('method-override')
+const cors = require('cors')
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/public'))
+app.use(layouts)
+app.use(methodOverride('_method'))
+
+
 
 app.use(
 	session({
@@ -22,43 +31,256 @@ app.use(
 
 app.use(flash())
 
-app.use(cookieParser())
-
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use(
-	cors({
-		origin: process.env.CLIENT_URL,
-		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-		credentials: true
-	})
-)
+// app.use(
+// 	cors({
+// 		origin: "http://localhost:3000", // allow to server to accept request from different origin
+// 		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+// 		credentials: true // allow session cookie from browser to pass through
+// 	})
+// );
 
-app.use('/auth', authRoutes)
+
+app.use((req, res, next) => {
+	// before every route, attach the flash messages and current user to res.locals
+	// res.locals.alerts = req.flash()
+	res.locals.currentUser = req.user
+	next()
+})
+
+app.use('/auth', require('./routes/auth'))
 
 const authCheck = (req, res, next) => {
-	if (!req.user) {
-		res.status(401).json({
-			authenticated: false,
-			message: "user has not been authenticated"
-		})
-	} else {
-		next()
-	}
+	next()
 }
 
-app.get('/', isLoggedIn, (req, res) => {
-	res.send({ message: "hello" })
+app.get('/', authCheck, (req, res) => {
+	res.render('index')
 })
 
-app.get('/imageprocessing', (req, res) => {
-	const imageURL = req.query.image
-	axios.get("http://localhost:5000/image", {
-		params: {
-			image: imageURL
+
+
+//--------checks if user is signed in and grabs all comments from comment database 
+
+
+app.get('/profile', isLoggedIn, async (req, res) => {
+
+	const comments = await db.comment.findAll({
+		where: {
+			userId: req.user.id
 		}
-	}).then(FlaskResponse => res.send(FlaskResponse.data))
+	})
+	console.log(comments)
+	res.render('profile', {
+		comments,
+		user: req.user
+	})
 })
 
-app.listen(process.env.PORT || 4000, () => { console.log(`Server is running on port ${process.env.PORT || 4000}`) })
+
+
+//--------
+
+app.get('/results', (req, res) => {
+	const queryString = {
+		params: {
+			q: req.query.track,
+			type: req.query.type,
+			limit: req.query.limit,
+		},
+	}
+	axios
+		.get(
+			`https://api.spotify.com/v1/search?q=${queryString.params.q}&type=${queryString.params.type}&limit=${queryString.params.limit}`, {
+			headers: {
+				Authorization: `Bearer ${req.user.access}`,
+			},
+		}
+		)
+		.then(function (response) {
+			res.render('results', {
+				data: response.data,
+				params: queryString.params,
+			})
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+})
+
+
+
+//--------
+
+
+app.get('/tracks/:id', function (req, res) {
+	const queryString = {
+		params: {
+			id: req.params.id,
+		},
+	}
+
+	axios
+		.get(`https://api.spotify.com/v1/tracks/${queryString.params.id}`, {
+			headers: {
+				Authorization: `Bearer ${req.user.access}`,
+			},
+		})
+		.then(function (spotifyResponse) {
+			db.comment
+				.findAll({
+					where: { trackId: spotifyResponse.data.id },
+					include: [db.user],
+				})
+				.then((allComments) => {
+					// res.send(allComments)
+					res.render('track', {
+						comments: allComments,
+						track: spotifyResponse.data,
+					})
+				})
+				.catch((error) => {
+					console.log(error)
+				})
+		})
+})
+
+
+
+//--------
+
+
+
+app.get('/artists/:id', function (req, res) {
+	const queryString = {
+		params: {
+			id: req.params.id,
+		},
+	}
+
+	axios
+		.get(`https://api.spotify.com/v1/artists/${queryString.params.id}`, {
+			headers: {
+				Authorization: `Bearer ${req.user.access}`,
+			},
+		})
+		.then(function (artistResponse) {
+			axios
+				.get(
+					`https://api.spotify.com/v1/artists/${queryString.params.id}/top-tracks?country=US`, {
+					headers: {
+						Authorization: `Bearer ${req.user.access}`,
+					},
+				}
+				)
+				.then(function (topTracksResponse) {
+					res.render('artist', {
+						artist: artistResponse.data,
+						tracks: topTracksResponse.data.tracks,
+					})
+				})
+			// db.comment
+			//   .findAll({ where: { trackId: spotifyResponse.data.id } })
+			//   .then((allComments) => {
+			//     res.render('track', {
+			//       comments: allComments,
+			//       track: spotifyResponse.data,
+			//     })
+			//   })
+		})
+})
+
+
+//-------- add usnique user to comment
+
+app.post('/track', (req, res) => {
+	db.user
+		.findOne({ where: { spotifyId: req.user.spotifyId } })
+		.then(function (user) {
+			user
+				.createComment({
+					text: req.body.text,
+					userId: req.user.id,
+					trackId: req.body.spotifyId,
+				})
+				.then(function (comment) {
+					user.addComment(comment)
+					res.redirect('back')
+				})
+		})
+})
+
+
+
+// *** EDIT COMMENT
+app.get('/edit/:id', (req, res) => {
+	const id = req.params.id
+	console.log(chalk.red(id))
+	db.comment
+		.findOne({
+			where: { id: id },
+		})
+		.then((comment) => {
+			res.render('editcomment', {
+				comment: comment,
+			})
+		})
+})
+
+
+
+app.put('/comments/:id', (req, res) => {
+	const id = req.body.commentId
+
+	db.comment
+		.update({ text: req.body.text }, {
+			where: { id: id },
+		})
+		.then((comment) => {
+			res.redirect('/')
+		})
+})
+
+
+
+
+// *** DELETE COMMENT
+app.delete('/comment/:id', (req, res) => {
+	const id = req.params.id
+	db.comment
+		.destroy({
+			where: { id: id },
+		})
+		.then((deletedComment) => {
+			res.redirect('back')
+		})
+})
+
+//-------- renders about page 
+
+app.get('/about', isLoggedIn, function (req, res) {
+	res.render('about')
+})
+
+
+//-------- logs  user out 
+
+app.get('/logout', function (req, res) {
+	req.logout()
+	res.redirect('/')
+})
+
+
+
+
+
+var server = app.listen(process.env.PORT || 4000, () =>
+	console.log(
+		`🎧You're listening to the smooth sounds of port ${process.env.PORT || 4000
+		}🎧`
+	)
+)
+
+module.exports = server
