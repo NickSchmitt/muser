ecf6ea3e3e05c1faac62c44e20fb09fd67810ac2
nickschmitt <nickschmitt@gmail.com>
2021-09-08 17:20:33 -0700

    Mock comment data added to home feed

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,7 +1,16 @@
+import { comments } from '../../data/comments/comments.json'
+import { tracks } from '../../data/comments/commentsSongs.json'
+
+
 function Home() {
 	return (
 		<div>
-			<p>Home Feed</p>
+			<h1>Recent Comments</h1>
+			{comments.map(commentData => {
+				return (
+					<p>{commentData.text}</p>
+				)
+			})}
 		</div>
 	)
 }
